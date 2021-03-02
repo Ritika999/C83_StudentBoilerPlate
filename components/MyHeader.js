e81c298e3836b75b853d969973210ebd509f58d8
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2021-03-02 13:45:10 +0530

    Update MyHeader.js
    Bell icon and hamburger icon to be done in C84

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -5,24 +5,16 @@ import { View, Text, StyeSheet ,Alert} from 'react-native';
 
 
 const BellIconWithBadge=(props)=>{
-  return(
-    <View>
-      <Icon name='bell' type='font-awesome' color='#696969' size={25}
-        onPress={() =>props.navigation.navigate('Notification')}/>
-       <Badge
-        value="1"
-       containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
-    </View>
-  )
+
 }
 
 
 const MyHeader = props => {
   return (
     <Header
-      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
+      
       centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
-      rightComponent={<BellIconWithBadge {...props}/>}
+    
       backgroundColor = "#eaf8fe"
     />
   );
